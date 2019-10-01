2069a67318f348beb91be20bd2cfdec4c6aed45b
teodesson <teodesson@gmail.com>
2019-10-02 00:37:36 +0800

    add meeting-dashboard app

---
+++ meeting-dashboard/src/test/java/com/dariawan/MeetingDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dariawan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeetingDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
